9f82f6e84c9c59dfe31a10be1355523491bf4fa8
Fletcher Fowler <fletcherfowler@gmail.com>
2010-11-19 00:50:26 -0700

    Initial Commit

---
+++ Default.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="Default" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <div>
+        <table>
+            <tr>
+                <td>Username:</td>
+                <td><asp:TextBox ID="usernameTxt" runat="server"></asp:TextBox></td>
+            </tr>
+            <tr>
+                <td>Password:</td>
+                <td><asp:TextBox ID="passwordTxt" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+           
+            </table>
+            <asp:Button ID="login" runat="server" Text="Login" onclick="login_Click" />
+    </div>
+</asp:Content>
+
---
+++ Default.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void login_Click(object sender, EventArgs e)
+    {
+        /*Once they try to login, this is where you will call your DLL file to read the XML list of usernames and passwords. You will pass in the login information and it will return a User object.  The User object
+          needs to return Username, Password, and whether they are an Administrator or not.
+        */
+
+        /*Then just add the username, password, and administrator value to to the Session variables.  At the top of each page I'll 
+        just check to make sure that there is data in these two fields. If not, then we know they never logged in and we won't let them see the protected pages.*/
+        //Session["username"] = userObject.username; //String
+        //Session["password"] = userObject.password; //String
+        //Session["admin"] = userObject.admin; //Boolean
+    }
+}
